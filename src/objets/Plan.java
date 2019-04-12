0649cea9396050d25e726b6d4a3c6aab2219bad5
jerem <jerem@DESKTOP-HR1PVUD>
2019-04-12 12:17:22 +0200

    init

---
+++ src/objets/Plan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objets;
+
+/**
+ *
+ * @author jerem
+ */
+public class Plan extends Figures{
+
+    @Override
+    public boolean intersection(Rayon r, Double resultat) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private int a,b,c,d;
+    
+    public Plan(int  a, int b, int c, int d){
+        
+    }
+    
+}
